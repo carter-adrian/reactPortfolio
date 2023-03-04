8c2364438b75b0673b6958593f442f3840420e55
unknown <carter.adrian@gmail.com>
2023-03-04 11:24:14 -0500

    added NavBar.jsx and name

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {FaBars, FaTimes} from 'react-icons/fa'
+
+const NavBar = () => {
+  return (
+    <div className= "flex justify-between items-center w-full h20 text-white bg-black px-4 fixed">
+        <div>
+            <h1 className= "text-5xl font-signature ml-2">Adrian Carter</h1>
+        </div>
+        <ul>
+            
+        </ul>
+    </div>
+  )
+}
+
+export default NavBar
