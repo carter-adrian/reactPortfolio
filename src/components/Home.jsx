b092fdd5995484c284dc9989f32d1fa62c7a7a12
unknown <carter.adrian@gmail.com>
2023-03-04 22:27:22 -0500

    added SocialLinks and img

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,25 +2,32 @@ import React from 'react'
 import heroImage from "../assets/heroImage.png";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
+
 const Home = () => {
   return (
-    <div name="home" className='flex h-screen w-full bg-gradient-to-b from-black to-gray-800'>
+    <div name="home" className='flex h-screen w-full bg-gradient-to-b from-black via-black to-gray-800' >
 
         <div className='max-w-screen-lg mx-auto flex flex-col items-center h-full px-4 md:flex-row'>
-            <div>
-                <h2>I'm a Full Stack Developer</h2>
-                <p>           
+            <div className='flex flex-col justify-center h-full'>
+                <h2 className='text-4lx sm:text-7xl font-bold text-white'>I'm a Full Stack Developer</h2>
+                <p className='text-gray-500 py-4 max-w-md'>           
                      I have 8 years of experience building and desgining software.
                      Currently, I love to work on web application using technologies like
                      React, Tailwind, Next JS and GraphQL.
                 </p>
 
                 <div>
-                    <button>
+                    <button className='group text-white w-fit py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500'>
                        Portfolio
-                       <MdOutlineKeyboardArrowRight />
+                       <span className='group-hover:rotate-90 duration-300'>
+                       <MdOutlineKeyboardArrowRight size={25} className='ml-1'/>
+                       </span>
                     </button>
                 </div>
+            </div>
+            <div>
+               <img src={heroImage} alt="My Profile" 
+               className='mx-auto w-2/3 md:full rounded-3xl'/> 
             </div>
         </div>
 
