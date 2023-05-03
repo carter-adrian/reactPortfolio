2631a988795291642ab1b7ba00abedb8112fbdea
carter-adrian <110795638+carter-adrian@users.noreply.github.com>
2023-05-02 20:36:34 -0400

    Update SocialLinks.jsx

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -12,7 +12,7 @@ const SocialLinks = () => {
           LinkedIn <FaLinkedin size={30} />
         </>
       ),
-      href: "https://linkedin.com",
+      href: "https://www.linkedin.com/in/adrian-carter/",
       style: "rounded-tr-md",
     },
     {
@@ -22,7 +22,7 @@ const SocialLinks = () => {
           GitHub <FaGithub size={30} />
         </>
       ),
-      href: "https://github.com/theyashpatel",
+      href: "https://github.com/carter-adrian/",
     },
     {
       id: 3,
@@ -31,7 +31,7 @@ const SocialLinks = () => {
           Mail <HiOutlineMail size={30} />
         </>
       ),
-      href: "mailto:foo@gmail.com",
+      href: "mailto:carter.adrian@gmail.com",
     },
     {
       id: 4,
@@ -40,7 +40,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "/resume.pdf",
+      href: "../public/resume.pdf",
       style: "rounded-br-md",
       download: true,
     },
