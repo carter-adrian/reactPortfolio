1ffe37c28e41cc597589a60fbdaa6264b76c791c
carter-adrian <110795638+carter-adrian@users.noreply.github.com>
2023-05-14 13:17:08 -0400

    Update SocialLinks.jsx

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -40,7 +40,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "../public/resume.pdf",
+      href: "../public/Adrian Carter Resume.pdf",
       style: "rounded-br-md",
       download: true,
     },
